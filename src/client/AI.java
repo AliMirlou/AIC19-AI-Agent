1f50a1226e16543da6e97ccf78eb2b9c12428c81
Ali Mirlou <alimirlou@gmail.com>
2019-02-16 23:15:33 +0330

    Added simple non-random AI Agent

--- src/client/AI.java
+++ src/client/AI.java
@@ -2,46 +2,73 @@ package client;
 
 import client.model.*;
 
-import java.util.Random;
+import java.util.Arrays;
+import java.util.Comparator;
 
-public class AI
-{
+public class AI {
+	public void preProcess(World world) {
+	}
 
-    private Random random = new Random();
+	public void pickTurn(World world) {
+		world.pickHero(HeroName.values()[world.getCurrentTurn()]);
+	}
 
-    public void preProcess(World world)
-    {
-        System.out.println("pre process started");
-    }
+	public void moveTurn(World world) {
+		Hero[] heroes = world.getMyHeroes();
+		Cell[] positions = Arrays.stream(heroes).map(Hero::getCurrentCell).toArray(Cell[]::new);
 
-    public void pickTurn(World world)
-    {
-        System.out.println("pick started");
-        world.pickHero(HeroName.values()[world.getCurrentTurn()]);
-    }
+		for (Hero hero : heroes) {
+			Cell currentPosition = hero.getCurrentCell();
+			// If in objective zone, hold ground
+			if (currentPosition.isInObjectiveZone()) continue;
 
-    public void moveTurn(World world)
-    {
-        System.out.println("move started");
-        Hero[] heroes = world.getMyHeroes();
+			// Otherwise try to reach the nearest objective zone cell
+			Cell[] objectiveZone = Arrays.stream(world.getMap().getObjectiveZone()).sorted(Comparator.comparingInt(o -> world.manhattanDistance(currentPosition, o))).toArray(Cell[]::new);
+			// Try to select an objective zone and move toward it
+			for (Cell objective : objectiveZone) {
+				// Pass my hero positions as blocked cells so the path won't be blocked
+				Direction[] toTheObjective = world.getPathMoveDirections(currentPosition, objective, positions);
+				if (toTheObjective.length != 0)
+					world.moveHero(hero.getId(), toTheObjective[0]);  // TODO heroes should get closer to each other to cast defensive abilities
+			}
+		}
+	}
 
-        for (Hero hero : heroes)
-        {
-            world.moveHero(hero, Direction.values()[random.nextInt(4)]);
-        }
-    }
-
-    public void actionTurn(World world) {
-        System.out.println("action started");
-        Hero[] heroes = world.getMyHeroes();
-        Map map = world.getMap();
-        for (Hero hero : heroes)
-        {
-            int row = random.nextInt(map.getRowNum());
-            int column = random.nextInt(map.getColumnNum());
-
-            world.castAbility(hero, hero.getAbilities()[random.nextInt(3)], row, column);
-        }
-    }
-
+	public void actionTurn(World world) {
+		Hero[] heroes = Arrays.stream(world.getMyHeroes()).sorted(Comparator.comparingInt(Hero::getCurrentHP)).toArray(Hero[]::new);
+		Hero[] visibleEnemies = Arrays.stream(world.getOppHeroes()).filter(hero -> hero.getCurrentCell().isInVision()).sorted(Comparator.comparingInt(Hero::getCurrentHP)).toArray(Hero[]::new);
+		heroes:
+		for (Hero hero : heroes) {
+			Cell myPosition = hero.getCurrentCell();
+			switch (hero.getName()) {
+				case HEALER:
+					// Heal if there is a damaged hero
+					Ability heal = hero.getAbility(AbilityName.HEALER_HEAL);
+					for (Hero otherHero : heroes) {
+						// Heal the hero if lost HP is higher than heal power (so it won't be wasted)
+						if (otherHero.getMaxHP() - otherHero.getCurrentHP() >= heal.getPower()) {
+							Cell otherHeroPosition = otherHero.getCurrentCell();
+							if (world.manhattanDistance(myPosition, otherHeroPosition) <= heal.getRange()) {
+								world.castAbility(hero.getId(), heal.getName(), otherHeroPosition.getRow(), otherHeroPosition.getColumn());
+								continue heroes;
+							}
+						}
+					}
+			}
+			// Attack with whatever you got
+			for (Ability ability : hero.getOffensiveAbilities()) {
+				if (ability.isReady()) {
+					for (Hero enemy : visibleEnemies) {
+						Cell enemyPosition = enemy.getCurrentCell();
+						if (world.isInVision(hero.getCurrentCell(), enemy.getCurrentCell())) {
+							if (world.getAbilityTargets(ability.getName(), myPosition, enemyPosition).length != 0) {
+								world.castAbility(hero.getId(), ability.getName(), enemyPosition.getRow(), enemyPosition.getColumn());
+								continue heroes;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
