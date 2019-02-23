9ea5959e4e4600e7745a0f2bdcb4434be4f18117
Ali Mirlou <alimirlou@gmail.com>
2019-02-23 17:16:44 +0330

    Dodge, dodge, dodge...

--- src/client/AI.java
+++ src/client/AI.java
@@ -4,11 +4,14 @@ import client.model.*;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class AI {
 	private int phase = 0;
+	private int AP = -1;
+	private HashMap<Integer, Cell> dodgeInsteadOfMove;  // Keys are hero IDs and values are the destination of dodge
 
-	private Cell getNewPosition(Map map, Cell cell, Direction direction) {
+	private Cell getNewPosition(Cell cell, Direction direction, Map map) {
 		int rowChange = 0, columnChange = 0;
 		switch (direction) {
 			case DOWN:
@@ -27,6 +30,7 @@ public class AI {
 	}
 
 	public void preProcess(World world) {
+		dodgeInsteadOfMove = new HashMap<>();
 	}
 
 	public void pickTurn(World world) {
@@ -34,15 +38,17 @@ public class AI {
 	}
 
 	public void moveTurn(World world) {
-		phase++;
+		++phase;
 		Hero[] heroes = world.getMyHeroes();
 		Cell[] positions = Arrays.stream(heroes).map(Hero::getCurrentCell).toArray(Cell[]::new);
-		int AP = world.getAP();
+		AP = (AP == -1) ? world.getAP() : AP;
 		Map map = world.getMap();
 
+		heroes:
 		for (int i = 0; i < heroes.length; ++i) {
 			Hero hero = heroes[i];
-			if (hero.getCurrentHP() != 0 && AP < hero.getMoveAPCost()) continue;
+			int ID = hero.getId();
+			if (hero.getCurrentHP() == 0 || AP < hero.getMoveAPCost() || dodgeInsteadOfMove.containsKey(ID)) continue;
 			Cell currentPosition = positions[i];
 
 			// If in objective zone, hold ground
@@ -53,18 +59,44 @@ public class AI {
 
 			// Try to select an objective zone and move toward it
 			for (Pair objective : objectiveZone) {  // FIXME spread around the zone
-				// Pass my hero positions as blocked cells so the path won't be blocked
-				Direction[] toTheObjective = world.getPathMoveDirections(currentPosition, (Cell) objective.getFirst(), positions);
-				if (toTheObjective.length != 0) {
-					world.moveHero(hero.getId(), toTheObjective[0]);  // TODO heroes should get closer to each other to cast defensive abilities
+				Cell destination = (Cell) objective.getFirst();
+				int distance = (Integer) objective.getSecond();
 
-					// Update AP changes
+				// Pass my hero positions as blocked cells so the path won't be blocked
+				Direction[] toTheObjective = world.getPathMoveDirections(currentPosition, destination, positions);
+				if (toTheObjective.length != 0) {
+					// Check if it is worth to not to move and instead dodge at action phase
+					Ability dodge = hero.getDodgeAbilities()[0];
+					if (phase == 1 && AP >= dodge.getAPCost() && dodge.isReady() && toTheObjective.length - (7 - phase) >= distance - dodge.getRange()) {
+						Cell nearer = currentPosition;
+						for (int j = 0; j < dodge.getRange(); ++j) {
+							Cell next = nearer;
+							int left = distance;
+							for (int k = 0; k < 4; ++k) {
+								Cell temp = getNewPosition(nearer, Direction.values()[k], map);
+								int leftTemp = world.manhattanDistance(destination, temp);
+								if (leftTemp < left) {
+									next = temp;
+									left = leftTemp;
+								}
+							}
+							nearer = next;
+						}
+						if (!nearer.isWall() && world.getPathMoveDirections(nearer, destination, positions).length < toTheObjective.length) {
+							dodgeInsteadOfMove.put(ID, nearer);
+							AP -= dodge.getAPCost();
+							continue heroes;
+						}
+					}
+					world.moveHero(ID, toTheObjective[0]);  // TODO heroes should get closer to each other to cast defensive abilities
+
+					// Update AP
 					AP -= hero.getMoveAPCost();
 
-					// Update position changes
-					positions[i] = getNewPosition(map, currentPosition, toTheObjective[0]);
+					// Update hero position
+					positions[i] = getNewPosition(currentPosition, toTheObjective[0], map);
 
-					break;
+					continue heroes;
 				}
 			}
 		}
@@ -74,39 +106,57 @@ public class AI {
 		phase = 0;
 		Hero[] heroes = Arrays.stream(world.getMyHeroes()).sorted(Comparator.comparingInt(Hero::getCurrentHP)).toArray(Hero[]::new);
 		Hero[] visibleEnemies = Arrays.stream(world.getOppHeroes()).filter(hero -> hero.getCurrentCell().isInVision()).sorted(Comparator.comparingInt(Hero::getCurrentHP)).toArray(Hero[]::new);
+
+		// First perform the scheduled dodges
+		for (int ID : dodgeInsteadOfMove.keySet()) {
+			Cell dest = dodgeInsteadOfMove.get(ID);
+			world.castAbility(ID, world.getHero(ID).getDodgeAbilities()[0].getName(), dest.getRow(), dest.getColumn());
+		}
+
 		heroes:
 		for (Hero hero : heroes) {
+			int ID = hero.getId();
 			Cell myPosition = hero.getCurrentCell();
+
+			// If hero has performed dodge earlier, then it can't perform any other abilities
+			if (dodgeInsteadOfMove.containsKey(ID)) {
+				dodgeInsteadOfMove.remove(ID);
+				continue;
+			}
+
 			switch (hero.getName()) {
 				case HEALER:
 					// Heal if there is a damaged hero
 					Ability heal = hero.getAbility(AbilityName.HEALER_HEAL);
+					if (!heal.isReady()) break;
 					for (Hero otherHero : heroes) {
-						// Heal the hero if lost HP is higher than heal power (so it won't be wasted)
+						// Heal the other hero if lost HP is more than heal power (so it won't be wasted)
 						if (otherHero.getMaxHP() - otherHero.getCurrentHP() >= heal.getPower()) {
 							Cell otherHeroPosition = otherHero.getCurrentCell();
 							if (world.manhattanDistance(myPosition, otherHeroPosition) <= heal.getRange()) {
-								world.castAbility(hero.getId(), heal.getName(), otherHeroPosition.getRow(), otherHeroPosition.getColumn());
+								world.castAbility(ID, heal.getName(), otherHeroPosition.getRow(), otherHeroPosition.getColumn());
 								continue heroes;
 							}
 						}
 					}
 				case GUARDIAN:  // TODO low range so they should get close to enemy... need support
 			}
+
 			// Attack with whatever you got
 			for (Ability ability : hero.getOffensiveAbilities()) {
-				if (ability.isReady()) {
-					for (Hero enemy : visibleEnemies) {
-						Cell enemyPosition = enemy.getCurrentCell();
-						if (world.isInVision(hero.getCurrentCell(), enemy.getCurrentCell())) {
-							if (world.getAbilityTargets(ability.getName(), myPosition, enemyPosition).length != 0) {
-								world.castAbility(hero.getId(), ability.getName(), enemyPosition.getRow(), enemyPosition.getColumn());
-								continue heroes;
-							}
+				if (!ability.isReady()) continue;
+				for (Hero enemy : visibleEnemies) {
+					Cell enemyPosition = enemy.getCurrentCell();
+					if (world.isInVision(hero.getCurrentCell(), enemy.getCurrentCell())) {
+						if (world.getAbilityTargets(ability.getName(), myPosition, enemyPosition).length != 0) {
+							world.castAbility(ID, ability.getName(), enemyPosition.getRow(), enemyPosition.getColumn());
+							continue heroes;
 						}
 					}
 				}
 			}
 		}
+
+		AP = -1;
 	}
 }
